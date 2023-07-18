6c0e2fc56d117a58747b95c7dc8ed16791fa8d2c
Utsav Patel <utsavpatel00265@gmail.com>
2023-07-18 23:05:18 +0530

    Navbar and basic content
    Added navbar to all pages.
    Added content for resume and contact file (basic).
    Added empty certificates page.

--- src/js/NavToggleSmallScreen.js
+++
@@ -1,6 +0,0 @@
-function toggleMenu() {
-    var menu = document.querySelector('.navbar__menu');
-    menu.classList.toggle('active');
-    var icon = document.querySelector('.navbar__icon');
-    icon.classList.toggle('active');
-}
